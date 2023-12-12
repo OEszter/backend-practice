d89aeb5d5353ad2d1b6ee7dc07a555705d315bbf
Eszter <szteran@gmail.com>
2023-12-12 11:50:25 +0100

    started delete endpoint

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,1 +1,87 @@
-console.log("loaded")
+const rootElement = document.querySelector("#root")
+let usersData = []
+
+const skeleton = () => `
+  <header></header>
+  <div class="cards"></div>
+`
+
+const formComponent = () => `
+  <form>
+    <div>
+      <input type="text" name="name" placeholder="enter name">
+      <input type="password" name="password" placeholder="enter password" required>
+    </div>
+
+    <button>send</button>
+  </form>
+`
+
+const userCardComponent = (userData) => `
+  <div class="card">
+    <h3>${userData.id}</h3>
+    <h2>${userData.name}</h2>
+  </div>
+`
+
+const init = () => {
+  rootElement.insertAdjacentHTML("beforeend", skeleton())
+
+  const headerElement = document.querySelector("header")
+  const cardsElement = document.querySelector(".cards")
+
+  headerElement.insertAdjacentHTML("beforeend", formComponent())
+
+  const inputNameElement = document.querySelector('input[name="name"]')
+  console.log(inputNameElement)
+  inputNameElement.addEventListener('input', (event) => {
+    if (event.data === "0" || event.data === "1" || event.data === "2") {
+      console.log('cant write number')
+      event.target.value = event.target.value.substring(0, event.target.value.length - 1)
+      event.target.disabled = true
+    }
+  })
+
+  const formElement = document.querySelector('form')
+  formElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const userName = document.querySelector(`input[name="name"]`).value
+    const userPassword = document.querySelector(`input[name="password"]`).value
+
+    fetch('/users/new-user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: userName,
+        password: userPassword
+      })
+    })
+      .then(res => {
+        if (res.status === 201) return res.json()
+        else throw Error('error at writing file')
+      })
+      .then(newUser => {
+          usersData.push(userCardComponent(newUser))
+          console.log(usersData)
+
+          cardsElement.insertAdjacentHTML('beforeend', usersData[usersData.length - 1])
+        }
+      )
+      .catch(err => console.log(err))
+
+  })
+
+  fetch('/users')
+    .then(res => res.json())
+    .then(data => {
+      usersData = data.map(user => userCardComponent(user))
+      // console.log(usersData)
+
+      cardsElement.insertAdjacentHTML("beforeend", usersData.join(""))
+    })
+}
+
+init()
--- index.js
+++ index.js
@@ -1,68 +1,165 @@
 /* IMPORTS */
-const express = require('express')  //az express csomagot importáljuk
-const path = require('path') //beimportáljuk a path modult
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
 
+/* app néven elindítjuk az express modulunkat */
+const app = express()
 
-const app = express() /* APP NÉVEN ELINDÍTJUK AZ EXPRESS MODULUNKAT */
-const port = 3000 //meghatározzuk a "port" változót
+/* meghatározzuk a "port" változót */
+const port = 3000
 
+/* middleware, ami parse-olja a json-t -> ehhez a requestnél be kell állítani a headers-ben a Content-Type: application/json headert */
+app.use(express.json())
 
-/* A localhost:port vagy 127.0.0.1:port/ felkeresésekor elérhetővé tesszük az index html fájlunkat */
+/* a localhost:port/ vagy 127.0.0.1:port/ felkeresésekor elérhetővé tesszük az index.html fájlunkat*/
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/index.html')) //elküldjök az adott helyen lévő fájlunkat
+  /* elküldjük az adott helyen lévő fájlunkat */
+  res.sendFile(path.join(__dirname, '/frontend/index.html'))
 })
 
-/* A localhost:port vagy 127.0.0.1:port/ felkeresésekor elérhetővé tesszük az script js fájlunkat */
-//az első paraméter  mindig a request, a második a respons - más nevet is adhatok neki, de ezt fogja jelenteni
-//adhatnám neki azt is kifelé, hogy app.get('/kismacska.css', .... Ha a köv. sorban style.css van, ami létezik )
-//A html és az index.js viszont egyezzen! 
+/* a localhost:port/style.css vagy 127.0.0.1:port/style.css felkeresésekor elérhetővé tesszük a style.css fájlunkat*/
 app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/static/css/style.css'))
+  res.sendFile(path.join(__dirname, '/frontend/static/css/style.css'))
 })
 
 app.get('/script.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/static/js/script.js'))
+  res.sendFile(path.join(__dirname, '/frontend/static/js/script.js'))
 })
 
-
-app.get('/users/:userid', (req, res) => {
-    res.send(req.params.userid) // : utáni rész userid néven jön létre a backenden a request paramsben
-    /* a : létrejött nem fix, az adott helyen jön létre, ez változhat
-    Ahol nincs kettőspont, az fix dolog. */
-    //const userId = Number(req.params.userid) //ez és a következő sor is használható, ugyanazt az eredményt kapjuk
-    const userId = parseInt(req.params.userid)
-    if (isNaN(userId)) {
-        console.log('userId must be a number') 
-        console.log()
-    } else {
-        console.log('reading file...')
-    }
-    console.log(userId)
-    
-    /* beolvasom a ... fájlt */
-    fs.readFile('text.txt', "utf8", (err, data) => { /* a paraméterek: fájl elérési útja/neve, kódolás, cb függvény, amiben 2 paramétert várunk: error object, data object */
-        /* hibakezelés, ha nem tud lefutni a beolvasás */
-        
-
-
-        
-    })
-})
-
-
-/* a  /public címen elérhetővé tesszök a /frontend/static mappánk tartalmát. MAszkolunk
-Maga a mappa nem lesz látható, csak a tartalma */
+/* /public címen elérhetővé tesszük a /frontend/static mappánk tartalmát */
 app.use('/public', express.static(path.join(__dirname, '/frontend/static')))
 
-
-
 app.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname, '/data/users.json'))
+  res.sendFile(path.join(__dirname, '/data/users.json'))
 })
 
+app.get('/users/:userid', (req, res) => {
+  // res.send(req.params.userid)
+  // const userId = Number(req.params.userid)
 
+  /* a bejövő paramétert átalakítom számmá (megpróbálom) */
+  const userId = parseInt(req.params.userid)
 
-/* elkezdi figyelni az adott portot a számítógépen(localhost vagy 127.0.0.1) */
+  /* lecsekkolom, hogy sikerült-e átalakítani számmá */
+  if (isNaN(userId)) {
+    console.log(`userid is not a number: ${req.params.userid}`)
+
+    /* ha nem sikerült, küldök egy hibát a frontendnek */
+    res.status(400).json('userId must be a number!!!')
+
+  } else { /* ha sikerült átalakítani, futtatom tovább a kódot */
+    console.log('reading file...')
+
+    /* beolvasom a users.json fájlt */
+    fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+      /* ha hibát találtam a fájl olvasás közben */
+      if (err) {
+        console.log('error at reading file: ', err)
+
+        res.status(500).json('error at reading file')
+      } else { /* ha nincs hiba, akkor van data */
+        console.log(`reading file was successful, searching for user id: ${userId}`)
+
+        /* a data értéke STRING, átalakítom JS-ben használható típussá (object, array) */
+        const users = JSON.parse(data)
+
+        /* megkeresem az adott userId-t a users adatban */
+        const foundUser = users.find((user) => user.id === userId)
+
+        if (foundUser) { /* TRUTHY értéket keresek pl. object { id: 2, name: 'John Doe' } */
+          console.log(`found user id: ${userId}, data: ${foundUser}`)
+
+          res.status(200).json(foundUser)
+        } else { /* FALSY értéke van pl. undefined */
+          console.log(`user id: ${userId} was not found`)
+
+          /* ha a find undefined-dal tér vissza, akkor nincs adott ID a users-ben */
+          res.status(404).json(`user id: ${userId} was not found`)
+        }
+      }
+    })
+  }
+})
+
+app.post('/users/new-user', (req, res) => {
+  /* a beérkezett adat a request body-jában található, tehát req.body === beérkezett adat */
+  const newUserData = req.body
+
+  /* beolvasom a jelenlegi users json-ömet */
+  fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+    /* hibakezelés */
+    if (err) {
+      console.log('error at reading file: ', err)
+
+      res.status(500).json('error at reading file')
+    } else {
+      /* átalakítom a beolvasott stringet json-né */
+      const users = JSON.parse(data) // users === array !!!
+
+      /* létrehozom az új user objektumot */
+      const newUser = {
+        id: users[users.length - 1].id + 1,
+        name: newUserData.name
+      }
+
+      /* kibővítem a users array-em */
+      users.push(newUser)
+
+      /* kiírom a fájlba a kibővített users array-t */
+      fs.writeFile(path.join(__dirname, '/data/users.json'), JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          console.log(`error at writing file: ${err}`)
+    
+          res.json(`error at writing file: ${err}`)
+        } else {
+          /* ez az a pont, amikor tudom, hogy sikeres volt minden lépés */
+          console.log(`successfully updated users with: ${JSON.stringify(newUser)}`)
+    
+          res.status(201).json(newUser)
+        }
+      })
+    }
+  })
+})
+
+app.delete('/users/delete', (req, res) => {
+  console.log(req.body)
+
+  fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.log(`error at reading file: ${err}`)
+
+      res.json(err)
+    } else {
+      const users = JSON.parse(data)
+      let deletedUser
+      console.log(users)
+
+      const newUsers = []
+
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].id !== req.body.id) {
+          newUsers.push(users[i])
+        } else {
+          deletedUser = users[i]
+        }
+      }
+
+      console.log(newUsers)
+
+      if (deletedUser) {
+        console.log(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
+        res.json(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
+      } else {
+        console.log(`user: ${req.body.id} not found`)
+        res.json(`user: ${req.body.id} not found`)
+      }
+    }
+  })
+})
+
+/* elkezdi figyelni az adott portot a számítógépen (localhost vagy 127.0.0.1) */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

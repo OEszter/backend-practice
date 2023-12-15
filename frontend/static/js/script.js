3075bd2a3f9f944e6b8969a0e8273fc1e6eee248
Eszter <szteran@gmail.com>
2023-12-15 11:52:27 +0100

    finished patch

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -20,29 +20,22 @@ const formComponent = () => `
 const userCardComponent = (userData) => `
   <div class="card">
     <h3>${userData.id}</h3>
-    <h2>${userData.name}</h2>
+    <h2 class="name">${userData.name}</h2>
+    <h4>${userData.age}</h4>
+    <span id="deleteid-${userData.id}" class="material-symbols-outlined delete">delete</span>
+    <span id="editid-${userData.id}" class="material-symbols-outlined edit">edit</span>
   </div>
 `
 
-const init = () => {
-  rootElement.insertAdjacentHTML("beforeend", skeleton())
-
-  const headerElement = document.querySelector("header")
-  const cardsElement = document.querySelector(".cards")
-
-  headerElement.insertAdjacentHTML("beforeend", formComponent())
-
+const formEventsComponent = (formElement, cardsElement) => {
   const inputNameElement = document.querySelector('input[name="name"]')
-  console.log(inputNameElement)
   inputNameElement.addEventListener('input', (event) => {
     if (event.data === "0" || event.data === "1" || event.data === "2") {
       console.log('cant write number')
       event.target.value = event.target.value.substring(0, event.target.value.length - 1)
-      event.target.disabled = true
     }
   })
 
-  const formElement = document.querySelector('form')
   formElement.addEventListener('submit', (event) => {
     event.preventDefault()
 
@@ -63,25 +56,119 @@ const init = () => {
         if (res.status === 201) return res.json()
         else throw Error('error at writing file')
       })
-      .then(newUser => {
-          usersData.push(userCardComponent(newUser))
-          console.log(usersData)
+      .then(resJson => {
+        /* usersData.push(userCardComponent(newUser))
 
-          cardsElement.insertAdjacentHTML('beforeend', usersData[usersData.length - 1])
-        }
-      )
+        cardsElement.insertAdjacentHTML('beforeend', usersData[usersData.length - 1]) */
+
+        console.log(resJson)
+        fetchUsers(cardsElement)
+      })
       .catch(err => console.log(err))
-
   })
+}
+
+const fetchUsers = (cardsElement) => {
+  cardsElement.innerHTML = ""
+  usersData = []
 
   fetch('/users')
     .then(res => res.json())
     .then(data => {
       usersData = data.map(user => userCardComponent(user))
       // console.log(usersData)
-
       cardsElement.insertAdjacentHTML("beforeend", usersData.join(""))
+
+      const deleteElements = document.querySelectorAll('span.delete')
+      deleteElements.forEach(deleteButton => deleteButton.addEventListener('click', () => {
+        const deleteId = deleteButton.id.split('-')[1]
+
+        fetch('/users/delete', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ id: deleteId })
+        })
+          .then(res => {
+            if (res.status === 200) return res.json()
+            else throw Error(`error at deleting user: ${deleteId}`)
+          })
+          .then(resJson => {
+            console.log(resJson)
+
+            fetchUsers(cardsElement)
+          })
+          .catch(err => {
+            rootElement.insertAdjacentHTML("afterbegin", `
+            <div class="error-box">
+              ${err}
+            </div>`)
+          })
+      }))
+
+      const editElements = document.querySelectorAll('span.edit')
+      editElements.forEach(editElement => editElement.addEventListener('click', () => {
+        //console.log(editElement.parentElement)
+
+        const cardElement = editElement.parentElement
+
+        const h2Element = cardElement.querySelector('h2')
+        //console.log(h2Element)
+
+        const userId = editElement.id.split('-')[1]
+        const oldName = h2Element.innerText
+
+        h2Element.innerHTML = `
+          <input type="text" name="name" placeholder="${oldName}">
+          <button id="id-${userId}" class="change">ok</button>
+        `
+
+        const changeButtonElement = cardElement.querySelector('button.change')
+        changeButtonElement.addEventListener('click', () => {
+          const inputElement = cardElement.querySelector('input[name="name"]')
+          const newName = inputElement.value // üres input stringnél value === ""
+          console.log(userId)
+          console.log(newName)
+
+          let newUserData = {
+            id: userId,
+            newData: {
+              name: newName
+            }
+          }
+
+          if (!newName) newUserData.newData.name = oldName
+
+          fetch('/users/patch', {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUserData)
+          })
+            .then(res => res.json())
+            .then(resJson => {
+              console.log(resJson)
+            
+              fetchUsers(cardsElement)
+            })
+        })
+      }))
     })
+}
+
+const init = () => {
+  rootElement.insertAdjacentHTML("beforeend", skeleton())
+
+  const headerElement = document.querySelector("header")
+  const cardsElement = document.querySelector(".cards")
+
+  headerElement.insertAdjacentHTML("beforeend", formComponent())
+  const formElement = document.querySelector('form')
+  formEventsComponent(formElement, cardsElement)
+
+  fetchUsers(cardsElement)
 }
 
 init()
--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post('/users/new-user', (req, res) => {
 
       /* létrehozom az új user objektumot */
       const newUser = {
-        id: users[users.length - 1].id + 1,
+        id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
         name: newUserData.name
       }
 
@@ -114,9 +114,9 @@ app.post('/users/new-user', (req, res) => {
           res.json(`error at writing file: ${err}`)
         } else {
           /* ez az a pont, amikor tudom, hogy sikeres volt minden lépés */
-          console.log(`successfully updated users with: ${JSON.stringify(newUser)}`)
+          console.log(`successfully created user: ${JSON.stringify(newUser)}`)
     
-          res.status(201).json(newUser)
+          res.status(201).json(`created user: ${JSON.stringify(newUser)}`)
         }
       })
     }
@@ -124,42 +124,136 @@ app.post('/users/new-user', (req, res) => {
 })
 
 app.delete('/users/delete', (req, res) => {
-  console.log(req.body)
+  const deleteId = parseInt(req.body.id)
 
   fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
     if (err) {
       console.log(`error at reading file: ${err}`)
 
-      res.json(err)
+      res.status(500).json(err)
     } else {
       const users = JSON.parse(data)
       let deletedUser
-      console.log(users)
 
       const newUsers = []
 
       for (let i = 0; i < users.length; i++) {
-        if (users[i].id !== req.body.id) {
+        if (users[i].id !== deleteId) {
           newUsers.push(users[i])
         } else {
           deletedUser = users[i]
         }
       }
 
-      console.log(newUsers)
-
       if (deletedUser) {
-        console.log(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
-        res.json(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
+        fs.writeFile(path.join(__dirname, '/data/users.json'), JSON.stringify(newUsers, 0, 2), (err) => {
+          if (err) {
+            console.log(`error at writing file: ${err}`)
+
+            res.status(500).json(err)
+          } else {
+            console.log(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
+            res.status(200).json(`deleted user: ${deletedUser.id}, deleted data: ${JSON.stringify(deletedUser)}`)
+          }
+        })
+
       } else {
-        console.log(`user: ${req.body.id} not found`)
-        res.json(`user: ${req.body.id} not found`)
+        console.log(`user: ${deleteId} not found`)
+        res.status(404).json(`user: ${deleteId} not found`)
       }
     }
   })
+})
+
+
+app.patch('/users/patch', (req, res) => {
+  const userId = parseInt(req.body.id)
+
+  if (isNaN(userId)) {
+    console.log(`user id must be a number, got: ${req.body.id}`)
+    res.json(`user id must be a number, got: ${req.body.id}`)
+  } else {
+    const newData = req.body.newData
+    const keysToChange = Object.keys(newData)
+
+    fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.log(`error at reading file: ${err}`)
+
+        res.status(500).json(err)
+      } else {
+        const users = JSON.parse(data)
+
+        const foundUser = users.find(user => user.id === userId)
+
+        if (foundUser) {
+          keysToChange.forEach(key => {
+            if (Object.keys(foundUser).includes(key)) {
+              foundUser[key] = newData[key]
+            } else {
+              console.log(`${key} is not a valid key on user`)
+            }
+          })
+
+          fs.writeFile(path.join(__dirname, '/data/users.json'), JSON.stringify(users, 0, 2), (err) => {
+            if (err) {
+              console.log(`error at writing file: ${err}`)
+
+              res.json(`error at reading file: ${err}`)
+            } else {
+              console.log(`user ${userId} has been successfully changed, new data: ${JSON.stringify(newData)}`)
+
+              res.json(`user ${userId} has been successfully changed, new data: ${JSON.stringify(newData)}`)
+            }
+          })
+
+        } else {
+          console.log(`user ${userId} is not found`)
+
+          res.json(`user ${userId} is not found`)
+        }
+      }
+    })
+
+    /* fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.log(`error at reading file: ${err}`)
+
+        res.status(500).json(err)
+      } else {
+        const users = JSON.parse(data)
+
+        //console.log('original users', users)
+
+        const foundUser = users.find(user => user.id === userId)
+        //console.log('found user', foundUser)
+        foundUser.name = newName
+        //console.log('changed user', foundUser)
+
+        //console.log('changed users? ', users)
+
+        fs.writeFile(path.join(__dirname, '/data/users.json'), JSON.stringify(users, 0, 2), (err) => {
+          if (err) {
+            console.log(`error at writing file: ${err}`)
+
+            res.status(500).json(err)
+          } else {
+            console.log(`successfully updated user ${userId}, with new data: ${newName}`)
+
+            res.status(202).json(`successfully updated user ${userId}, with new data: ${newName}`)
+          }
+        })
+      }
+    }) */
+  }
 })
 
 /* elkezdi figyelni az adott portot a számítógépen (localhost vagy 127.0.0.1) */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
